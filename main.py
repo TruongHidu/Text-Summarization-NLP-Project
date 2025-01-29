744feb3fee9fa08ee4c0252d928dd027bb89925b
TruongHidu <nguyenvantruong1009204@gmail.com>
2025-01-29 23:55:06 +0700

    requirements & installation done

---
+++ main.py
@@ -0,0 +1,6 @@
+from textSummarizer.logging import logger
+
+# Test log
+logger.info("This is an info message")
+logger.warning("This is a warning message")
+logger.error("This is an error message")
